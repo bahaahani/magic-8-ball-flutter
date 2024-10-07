87fad3c66d23afd72f689a76f4656a72b798dd76
bahaa shamtoot <blainefire00@gmail.com>
2024-10-07 22:35:51 +0300

    Add main functionality and UI for Magic 8 Ball app; update SDK version and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Ask Me Anything',
+              style: TextStyle(
+                color: Colors.white,
+              )),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: Ball());
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue.shade500,
+      child: Center(
+        child: TextButton(
+          onPressed: () {
+
+            setState(() {
+              ballNumber = Random.secure().nextInt(5) +1;
+            });
+          },
+          child: Image.asset('images/ball$ballNumber.png'),
+        ),
+      ),
+    );
+  }
+}
